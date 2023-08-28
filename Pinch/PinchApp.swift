a818b67976b670dbf76e187f662382230f6008eb
hyesungpark <jayhspark@gmail.com>
2023-08-28 21:53:48 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Hye Sung Park on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
