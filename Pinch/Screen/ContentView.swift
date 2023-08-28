9cfa8404e2ce1c0743805c019e1270ebfff949c8
hyesungpark <jayhspark@gmail.com>
2023-08-28 22:19:20 +0900

    add animation

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,25 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      // MARK: - PROPERTY
-      
-      // MARK: - FUNCTION
-      
-      // MARK: - CONTENT
-      
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  // MARK: - PROPERTY
+  
+  @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
+  
+  // MARK: - FUNCTION
+  
+  // MARK: - CONTENT
+  var body: some View {
+    
+    NavigationView {
+      ZStack {
+        // MARK: - PAGE IMAGE
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
+          .scaleEffect(imageScale)
+          .onTapGesture(count: 2) {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              withAnimation(.spring()){
+                imageScale = 1
+              }
+            }
+          }
+          .gesture(
+            DragGesture()
+              .onChanged({ value in
+                withAnimation(.linear(duration: 1)) {
+                  imageOffset = value.translation
+                }
+              })
+              .onEnded({ _ in
+                <#code#>
+              })
+          )
+          
+      }
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+    }// : NAVIGATION
+    .navigationViewStyle(.stack)
+    .onAppear {
+      withAnimation(.linear(duration: 1)) {
+        isAnimating = true
+      }
     }
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
