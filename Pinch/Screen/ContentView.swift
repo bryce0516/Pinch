570e9a022f9eac57dd6095f0a9424c7ea3d568d0
hyesungpark <jayhspark@gmail.com>
2023-08-29 23:29:12 +0900

    finish final touch

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -176,6 +176,12 @@ struct ContentView: View {
               .frame(width: 80)
               .cornerRadius(8)
               .shadow(radius: 4)
+              .opacity(isDrawerOpen ? 1 : 0)
+              .animation(.easeOut(duration: 0.5), value: isDrawerOpen)
+              .onTapGesture {
+                isAnimating = true
+                pageIndex = item.id
+              }
           }
           
           Spacer()
